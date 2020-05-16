e808a3d4b90e474fe9ce4103885dea59ea39072a
2opex <jerry1003tw@gmail.com>
2020-05-16 21:33:55 +0800

    navbar sticky & fixed

--- js/navbar.js
+++ js/navbar.js
@@ -4,5 +4,6 @@ function isfixed() {
     header_area.style.position = "sticky";
     header_area.style.backgroundColor = "#000000";
     header_area.style.top = "0";
+    header_area.style.zIndex = "10";
     window.removeEventListener('scroll', ifSticky, false);
 }
